f3394f6650a575e6b53fa676b9f78b1198af4350
JuneEnding <Uyrona@uandex.ru>
2024-03-31 22:52:23 +0300

    Audio Recorder

---
+++ AudioRecorder/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,51 @@
+﻿using ReactiveUI;
+using System;
+using System.Diagnostics;
+using System.Reactive;
+
+namespace AudioRecorder.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public ReactiveCommand<Unit, Unit> ExportCommand { get; }
+    public ReactiveCommand<Unit, Unit> PauseCommand { get; }
+    public ReactiveCommand<Unit, Unit> PlayCommand { get; }
+    public ReactiveCommand<Unit, Unit> RecordCommand { get; }
+    public ReactiveCommand<Unit, Unit> SaveCommand { get; }
+    public ReactiveCommand<Unit, Unit> StopCommand { get; }
+
+    public MainWindowViewModel()
+    {
+        ExportCommand = ReactiveCommand.Create(ExecuteExportCommand);
+        PauseCommand = ReactiveCommand.Create(ExecutePauseCommand);
+        PlayCommand = ReactiveCommand.Create(ExecutePlayCommand);
+        RecordCommand = ReactiveCommand.Create(ExecuteRecordCommand);
+        SaveCommand = ReactiveCommand.Create(ExecuteSaveCommand);
+        StopCommand = ReactiveCommand.Create(ExecuteStopCommand);
+    }
+
+    private void ExecutePauseCommand()
+    {
+        Debug.WriteLine("ExecutePauseCommand!!!");
+    }
+    private void ExecuteExportCommand()
+    {
+        Debug.WriteLine("ExecuteExportCommand!!!");
+    }
+    private void ExecutePlayCommand()
+    {
+        Debug.WriteLine("ExecutePlayCommand!!!");
+    }
+    private void ExecuteRecordCommand()
+    {
+        Debug.WriteLine("ExecuteRecordCommand!!!");
+    }
+    private void ExecuteSaveCommand()
+    {
+        Debug.WriteLine("ExecuteSaveCommand!!!");
+    }
+    private void ExecuteStopCommand()
+    {
+        Debug.WriteLine("ExecuteStopCommand!!!");
+    }
+}
---
+++ AudioRecorder/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace AudioRecorder.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
